6b516e1c0ade4735286351d5c1675476ddf5710b
Chr1sDev <chris@chr1s.dev>
2021-05-31 23:30:17 -0500

    add embed helper

---
+++ src/helpers/embed.ts
@@ -0,0 +1,35 @@
+module.exports = {
+  embed(
+    author,
+    authorImage,
+    title,
+    description,
+    fields,
+    footer,
+    image,
+    color,
+    thumbnail
+  ) {
+    const { MessageEmbed } = require("discord.js");
+    const embed = new MessageEmbed();
+
+    if (author) embed.setAuthor(author, authorImage ? authorImage : "");
+    if (title) embed.setTitle(title);
+    if (description) embed.setDescription(description);
+    if (fields) {
+      for (const field of fields) {
+        embed.addField({
+          name: field.name,
+          value: field.value,
+          inline: field.inline,
+        });
+      }
+    }
+    if (footer) embed.setFooter(footer);
+    if (image) embed.setImage(image);
+    if (color) embed.setColor(color);
+    if (thumbnail) embed.setThumbnail(thumbnail);
+
+    return embed;
+  },
+};
