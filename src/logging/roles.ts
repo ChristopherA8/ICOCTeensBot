d0d2607b2886f68ff7b8933af09ab8c0de448d81
Chr1sDev <chris@chr1s.dev>
2021-06-01 12:46:57 -0500

    add role logging

---
+++ src/logging/roles.ts
@@ -0,0 +1,64 @@
+module.exports = {
+  log(client) {
+    const { embed } = require("../helpers/embed.ts");
+    client.on("roleCreate", (newRole) => {
+      if (newRole.guild.id !== "698590629344575500") return;
+      const channel = newRole.guild.channels.cache.get("849126700763447367");
+      // embed(author, authorImage, title, description, fields, footer, image, color, thumbnail)
+      channel.send(
+        embed(
+          "Role Created",
+          null,
+          null,
+          `@${newRole.name}`,
+          null,
+          null,
+          null,
+          "#47a8e8",
+          null
+        )
+      );
+    });
+
+    client.on("roleDelete", (oldRole) => {
+      if (oldRole.guild.id !== "698590629344575500") return;
+      const channel = oldRole.guild.channels.cache.get("849126700763447367");
+      // embed(author, authorImage, title, description, fields, footer, image, color, thumbnail)
+      channel.send(
+        embed(
+          "Role Deleted",
+          null,
+          null,
+          `@${oldRole.name}`,
+          null,
+          null,
+          null,
+          "#47a8e8",
+          null
+        )
+      );
+    });
+
+    client.on("roleUpdate", (before, after) => {
+      if (before.guild.id !== "698590629344575500") return;
+      const channel = before.guild.channels.cache.get("849126700763447367");
+
+      if (before.name !== after.name) {
+        // embed(author, authorImage, title, description, fields, footer, image, color, thumbnail)
+        channel.send(
+          embed(
+            "Role Name Changed",
+            null,
+            null,
+            `\`${before.name}\` to \`${after.name}\``,
+            null,
+            null,
+            null,
+            "#47a8e8",
+            null
+          )
+        );
+      }
+    });
+  },
+};
