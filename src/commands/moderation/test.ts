799645adc43379e1e01f66008833aac6e633f0ab
Chr1sDev <chris@chr1s.dev>
2021-06-01 15:18:34 -0500

    test file :)

--- src/commands/moderation/test.ts
+++ src/commands/moderation/test.ts
@@ -2,7 +2,9 @@ module.exports = {
   name: "test",
   permissions: 9,
   execute(msg) {
-    const { embed } = require("../../helpers/embed.ts");
-    msg.channel.send(embed("ur mom", null, null, "hi", null, null, null, null));
+    let fields = [{ name: "name", value: "value", inline: true }];
+    for (const field of fields) {
+      console.log(field.name);
+    }
   },
 };
