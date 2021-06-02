923e37b0f82b06c05d6cb901daeb20b7279ab6a3
Chr1sDev <chris@chr1s.dev>
2021-06-01 21:13:06 -0500

    add clubs embed command for me

---
+++ src/commands/misc/clubs.ts
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "clubs",
+  permissions: 9,
+  execute(msg) {
+    const { MessageEmbed } = require("discord.js");
+    const embed = new MessageEmbed()
+      .setAuthor("React to join a club")
+      .setDescription(
+        "> ⛩️ - Anime Club\n> \n> 🎨 - Art Club\n> \n> 🏃‍♂️ - Athletics Club\n> \n> 📚 - Book Club\n> \n> 🎲 - D&D Club\n> \n> 🍜 - Food Club\n> \n> 🎮 - Gaming Club\n> \n> 📺 - Movie/TV Club\n> \n> 🎼 - Music Club\n> \n> 🐈 - Pet Club\n> \n> 🔨 - STEM Club\n> \n> 🧳 - Travel Club\n> \n> 🌳 - Nature Club\n\n**React again to leave a club**"
+      );
+    msg.channel.send(embed).then((msg) => {
+      msg.react("⛩️");
+      msg.react("🎨");
+      msg.react("🏃‍♂️");
+      msg.react("📚");
+      msg.react("🎲");
+      msg.react("🍜");
+      msg.react("🎮");
+      msg.react("📺");
+      msg.react("🎼");
+      msg.react("🐈");
+      msg.react("🔨");
+      msg.react("🧳");
+      msg.react("🌳");
+    });
+  },
+};
