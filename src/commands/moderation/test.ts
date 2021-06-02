5274383d71bb729f65fba93024dd059ebdf648ab
Chr1sDev <christopheranderson3@me.com>
2021-06-02 20:52:02 +0000

    test?

--- src/commands/moderation/test.ts
+++ src/commands/moderation/test.ts
@@ -2,9 +2,10 @@ module.exports = {
   name: "test",
   permissions: 9,
   execute(msg) {
-    let fields = [{ name: "name", value: "value", inline: true }];
-    for (const field of fields) {
-      console.log(field.name);
+    const birthdays = require("../../databases/birthdays.json");
+    for (const person of birthdays) {
+      console.log(JSON.stringify(person, null, 2));
     }
+    const fs = require("fs");
   },
 };
