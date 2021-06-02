c3e5fb909bf8783d0f18421499661f394e93a11b
Chr1sDev <christopheranderson3@me.com>
2021-06-02 20:51:58 +0000

    add birthday command

---
+++ src/commands/misc/birthday.ts
@@ -0,0 +1,34 @@
+module.exports = {
+  name: "birthday",
+  permissions: 2,
+  execute(msg, args) {
+    const fs = require("fs");
+    const data = fs.readFileSync("./src/databases/birthdays.json");
+    let birthdays = JSON.parse(data);
+    for (const person of birthdays)
+      if (person.id == msg.author.id) {
+        msg.reply(
+          "Your bithday is already added\nAsk christopher if you need to edit it"
+        );
+        return;
+      }
+
+    if (!args[0]) {
+      msg.reply("Don't forget the month and date, like this (2/28)");
+      return;
+    }
+
+    let newPerson = {
+      id: msg.author.id,
+      date: args[0],
+    };
+
+    birthdays.push(newPerson);
+    fs.writeFileSync(
+      "./src/databases/birthdays.json",
+      JSON.stringify(birthdays, null, 2)
+    );
+
+    msg.reply(`Birthday added ${args[0]}`);
+  },
+};
