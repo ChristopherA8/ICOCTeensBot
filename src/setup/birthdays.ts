71aa83479a44ffb4b679549a25177926e4130b4a
Chr1sDev <christopheranderson3@me.com>
2021-06-02 20:52:11 +0000

    birthday manager

---
+++ src/setup/birthdays.ts
@@ -0,0 +1,20 @@
+module.exports = {
+  async setup(client) {
+    /*     const guild = client.guilds.cache.get("698590629344575500");
+    const birthdayRole = guild.roles.cache.get("832022073366806531");
+    const fs = require("fs");
+
+    let now = new Date();
+    console.log(`${now.getMonth()}/${now.getDay()}`);
+
+    setInterval(() => {
+      let now = new Date();
+      console.log(`${now.getDay()}/${now.getMonth()}`);
+      let raw = fs.readFileSync("./src/databases/birthdays.json");
+      let birthdays = JSON.parse(raw);
+      for (const person of birthdays) {
+        // if (person.date == )
+      }
+    }, 60 * 60 * 1000); // 1 hr */
+  },
+};
