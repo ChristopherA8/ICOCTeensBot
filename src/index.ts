fae8110d53410f07695e98e7a05cde0cf3694a48
Chr1sDev <chris@chr1s.dev>
2021-06-02 00:35:36 -0500

    something?

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,15 @@ client.on(`ready`, () => {
   }
 });
 
+client.on("guildMemberAdd", (join) => {
+  if (join.guild.id !== `698590629344575500`) return;
+  const channel = join.guild.channels.cache.get(`698591277205422171`);
+  channel.send(`Welcome ${join} to ICOC Teens!`);
+  join.send(
+    `Welcome ${join} to ICOC Teens!\n\nThanks for checking out the server. To join the server please fill out this form!\n\n<https://docs.google.com/forms/d/e/1FAIpQLSfatFjGGgYmdMjsPFZKM-KX8zEuWvlKi76KX8XNceGTbEiMlw/viewform>\nIf you have any issues/questions filling out the form, feel free to dm a staff member`
+  );
+});
+
 client.on(`message`, async (msg) => {
   if (msg.author.bot) return;
   if (msg.channel.type === "dm") return;
