f92a06db9d598cb155e6547c0826f70cea0cbde7
Chr1sDev <christopheranderson3@me.com>
2021-06-03 18:35:08 +0000

    add xp slash command

---
+++ src/slash/xp.ts
@@ -0,0 +1,32 @@
+module.exports = {
+  name: "xp",
+  execute(interaction) {
+    const SQLite = require("better-sqlite3");
+    const sql = new SQLite("./src/databases/scores.sqlite");
+    const { MessageEmbed } = require("discord.js");
+
+    sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+    sql.prepare(
+      "INSERT OR REPLACE INTO scores (id, user, guild, points, level, name) VALUES (@id, @user, @guild, @points, @level, @name);"
+    );
+
+    let score = sql
+      .prepare("SELECT * FROM scores WHERE user = ? AND guild = ?")
+      .get(interaction.user.id, "698590629344575500");
+    if (!score) {
+      interaction.reply("Talk to get some xp");
+      return;
+    }
+    const embed = new MessageEmbed()
+      .setAuthor(
+        interaction.user.tag,
+        interaction.user.displayAvatarURL({ dynamic: true })
+      )
+      .addFields(
+        { name: "XP", value: `${score.points}`, inline: true },
+        { name: "Level", value: `${score.level}`, inline: true }
+      )
+      .setColor("#47a8e8");
+    interaction.reply(embed);
+  },
+};
