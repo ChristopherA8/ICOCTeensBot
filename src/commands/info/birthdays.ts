9d8aa716288a611930deb2022e43803fba73ab5b
Chr1sDev <christopheranderson3@me.com>
2021-06-03 00:26:28 +0000

    add birthdays command

---
+++ src/commands/info/birthdays.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "birthdays",
+  permissions: 1,
+  execute(msg) {
+    const fs = require("fs");
+    const { MessageEmbed } = require("discord.js");
+    let data = fs.readFileSync("./src/databases/birthdays.json");
+    let birthdays = JSON.parse(data);
+
+    function getMember(id) {
+      return msg.guild.members.cache.get(id);
+    }
+
+    const embed = new MessageEmbed().setAuthor("Birthdays").setColor("#47a8e8");
+
+    for (const person of birthdays) {
+      let member = getMember(person.id);
+      embed.addField(member.user.tag, person.date);
+    }
+    msg.reply(embed);
+  },
+};
