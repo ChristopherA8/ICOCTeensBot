81ee2fbbe974d061b9923e76ab8fdb777a67b58f
Chr1sDev <christopheranderson3@me.com>
2021-06-04 18:29:04 +0000

    add play voice command

--- src/commands/voice/play.ts
+++ src/commands/voice/play.ts
@@ -1,7 +1,16 @@
 module.exports = {
   name: "play",
   permission: 1,
+  category: "voice",
+  description: "Play music in vc",
   execute(msg, args) {
+    const voiceChannel = msg.member.voice.channel;
+    if (!voiceChannel) return msg.reply(`Please join a voice channel first!`);
+
+    if (!args[0]) {
+      msg.reply(`Missing song name`);
+      return;
+    }
     msg.client.distube.play(msg, args.join(" "));
   },
 };
