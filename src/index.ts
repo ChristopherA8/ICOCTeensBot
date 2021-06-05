a68f1fd483a0960f450ca68158b64d3ecbb87a08
Chr1sDev <christopheranderson3@me.com>
2021-06-05 03:44:03 +0000

    big dum

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,9 @@ client.on(`ready`, () => {
 client.on("guildMemberAdd", (join) => {
   if (join.guild.id !== `698590629344575500`) return;
   const channel = join.guild.channels.cache.get(`698591277205422171`);
-  channel.send(`Welcome to the ICOC Teens Server`);
+  channel.send(`Welcome <@${join.id}> to ICOC Teens!`);
   const welcomeEmbed = new MessageEmbed()
-    .setTitle(`Welcome <@${join.id}>!`)
+    .setTitle(`Welcome to the ICOC Teens Server`)
     .setDescription(
       `Thanks for joining the ICOC Teens Server. To gain access to the server please fill out this google form. It helps us keep track of who joins, so we can keep the server a safe place for everyone.\nForm Link: <https://docs.google.com/forms/d/e/1FAIpQLSfatFjGGgYmdMjsPFZKM-KX8zEuWvlKi76KX8XNceGTbEiMlw/viewform>\nIf you have any issue or questions regarding the form, don't hesitate to DM a staff member for assistance.`
     )
