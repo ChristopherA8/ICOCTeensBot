2c8c2ce09561fd118d37c66b9f86dc44196fe29c
Chr1sDev <christopheranderson3@me.com>
2021-06-14 17:24:41 +0000

    add XP Leader listneer

---
+++ src/listeners/topxp.ts
@@ -0,0 +1,26 @@
+module.exports = {
+  listen(msg) {
+    const SQLite = require("better-sqlite3");
+
+    // Create SQLite database
+    const sql = new SQLite("./src/databases/scores.sqlite");
+
+    const top = sql
+      .prepare(
+        "SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 1"
+      )
+      .get("698590629344575500");
+    const topMem = msg.guild.members.cache.get(top.id.substr(19));
+
+    if (!topMem.roles.cache.some((role) => role.id == `808429363392806952`)) {
+      topMem.roles.add(`808429363392806952`);
+      msg.guild.members.cache
+        .filter((mem) =>
+          mem.roles.cache.some((role) => role.id == `808429363392806952`)
+        )
+        .forEach((member) => {
+          member.roles.remove(`808429363392806952`);
+        });
+    }
+  },
+};
