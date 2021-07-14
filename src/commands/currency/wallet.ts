a326888ea5c6fed83186e5619a05e9a8cd9bc6de
Chr1sDev <christopheranderson3@me.com>
2021-07-14 01:15:42 +0000

    [Commands] Show your wallet balance

---
+++ src/commands/currency/wallet.ts
@@ -0,0 +1,40 @@
+module.exports = {
+  name: "wallet",
+  permissions: 1,
+  category: "currency",
+  description: "Show the users wallet balance",
+  async execute(msg) {
+    const { MessageEmbed } = require("discord.js");
+    const SQLite = require("better-sqlite3");
+    const { doIExist } = require("./doIExist.ts");
+    const walletDB = new SQLite("./src/databases/currency.sqlite");
+
+    const ping = msg.mentions.members.first();
+
+    await doIExist(msg.author.id, ping);
+
+    if (ping) {
+      let wallet = walletDB
+        .prepare("SELECT * FROM currency WHERE id = ?")
+        .get(ping.id);
+
+      const embed = new MessageEmbed()
+        .setTitle(`${ping.user.username}'s Wallet`)
+        .addFields({ name: "Balance", value: `$${wallet.money}`, inline: true })
+        .setColor(msg.author.displayColor);
+
+      return msg.reply(embed);
+    }
+
+    let wallet = walletDB
+      .prepare("SELECT * FROM currency WHERE id = ?")
+      .get(msg.author.id);
+
+    const embed = new MessageEmbed()
+      .setTitle(`${msg.author.username}'s Wallet`)
+      .addFields({ name: "Balance", value: `$${wallet.money}`, inline: true })
+      .setColor(msg.author.displayColor);
+
+    msg.reply(embed);
+  },
+};
