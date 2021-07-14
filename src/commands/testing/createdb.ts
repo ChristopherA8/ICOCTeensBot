8347d5f7417a5ced4a80dd07dde7de36e2a22c2f
Chr1sDev <christopheranderson3@me.com>
2021-07-14 01:15:53 +0000

    stuff

--- src/commands/testing/createdb.ts
+++ src/commands/testing/createdb.ts
@@ -3,13 +3,13 @@ module.exports = {
   permission: 9,
   execute(msg) {
     const SQLite = require("better-sqlite3");
-    const sql = new SQLite("./src/databases/currency.sqlite");
+    const sql = new SQLite("./src/databases/bank.sqlite");
 
     sql
-      .prepare("CREATE TABLE currency (id TEXT PRIMARY KEY, money INTEGER);")
+      .prepare("CREATE TABLE bank (id TEXT PRIMARY KEY, money INTEGER);")
       .run();
     // Ensure that the "id" row is always unique and indexed.
-    sql.prepare("CREATE UNIQUE INDEX idx_currency_id ON currency (id);").run();
+    sql.prepare("CREATE UNIQUE INDEX idx_bank_id ON bank (id);").run();
     sql.pragma("synchronous = 1");
     sql.pragma("journal_mode = wal");
   },
