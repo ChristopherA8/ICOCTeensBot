0764971efb1f117572321c1985d73a30afd8640b
Chr1sDev <christopheranderson3@me.com>
2021-07-16 20:51:15 +0000

    [Logging:Invites] cleanup code

--- src/logging/invites.ts
+++ src/logging/invites.ts
@@ -33,33 +33,5 @@ module.exports = {
         )
       );
     });
-
-    // client.on(`inviteCreate`, (inv) => {
-    //   if (inv.guild.id !== `698590629344575500`) return; // Ignore emoji servers
-
-    //   const channel = inv.client.channels.cache.find(
-    //     (channel) => channel.id === `759967435309842494`
-    //   );
-    //   createInv(inv, channel);
-
-    //   //////////////////
-    //   // EMBED
-    //   //////////////////
-
-    //   function createInv(inv, channel) {
-    //     const exampleEmbed = new Discord.MessageEmbed()
-    //       .setAuthor(`Invite Created -`)
-    //       .setColor("#00FF86")
-    //       .setFooter(`Invite Code: ${inv.code}`)
-    //       .addFields(
-    //         { name: `Invite URL`, value: `<${inv}>`, inline: true },
-    //         { name: `Invite Maker:`, value: `${inv.inviter}`, inline: false },
-    //         { name: `Max Uses:`, value: `${inv.maxUses}`, inline: true },
-    //         { name: `Length:`, value: `${inv.maxAge}`, inline: true }
-    //       );
-    //     //.setDescription(`Invite Created: ${inv}`)
-    //     channel.send(exampleEmbed);
-    //   }
-    // });
   },
 };
