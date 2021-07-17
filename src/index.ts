83fe24257efa3b77ee3888d2c3b909828dbb8784
Chr1sDev <christopheranderson3@me.com>
2021-07-17 19:59:30 +0000

    add events

--- src/index.ts
+++ src/index.ts
@@ -17,19 +17,15 @@ client.on(`ready`, () => {
     const { setup } = require(`../src/setup/${file}`);
     setup(client);
   }
-});
 
-client.on("guildMemberAdd", (join) => {
-  if (join.guild.id !== `698590629344575500`) return;
-  const channel = join.guild.channels.cache.get(`698591277205422171`);
-  channel.send(`Welcome <@${join.id}> to ICOC Teens!`);
-  const welcomeEmbed = new MessageEmbed()
-    .setTitle(`Welcome to the ICOC Teens Server`)
-    .setDescription(
-      `Thanks for joining the ICOC Teens Server. To gain access to the server please fill out this google form. It helps us keep track of who joins, so we can keep the server a safe place for everyone.\nForm Link: <https://docs.google.com/forms/d/e/1FAIpQLSfatFjGGgYmdMjsPFZKM-KX8zEuWvlKi76KX8XNceGTbEiMlw/viewform>\nIf you have any issue or questions regarding the form, don't hesitate to DM a staff member for assistance.`
-    )
-    .setColor("#47a8e8");
-  join.send(welcomeEmbed);
+  // Run events
+  const eventfiles = fs
+    .readdirSync(`./src/events`)
+    .filter((file) => file.endsWith(".ts"));
+  for (const file of eventfiles) {
+    const { event } = require(`../src/events/${file}`);
+    event(client);
+  }
 });
 
 client.on(`message`, async (msg) => {
