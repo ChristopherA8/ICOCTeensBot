11f897ff3da89bc5eb7d0eb2be6e2ba35a1deef3
Chr1sDev <christopheranderson3@me.com>
2021-10-18 01:28:49 +0000

    [Commands/@someone] whitelist jack

--- src/commands/misc/@someone.ts
+++ src/commands/misc/@someone.ts
@@ -2,8 +2,10 @@ module.exports = {
   name: "@someone",
   category: "misc",
   description: "Ping a random person",
-  permissions: 1,
+  permissions: 11, // Special permission 11 requires a memberID
+  memberIds: [544890228507410442], // Jack
   async execute(msg) {
+    msg.channel.send("ur mom");
     if (msg.author.id !== "544890228507410442") return;
     const members = await msg.guild.members.fetch();
     const randMember = members.random();
