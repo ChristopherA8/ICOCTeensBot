15715efe88af468aa0181ebbd2f84da868bc1957
Chr1sDev <christopheranderson3@me.com>
2021-10-27 02:33:52 +0000

    [Commands] user <t:432432:f> format for dates, years still broken tho

--- src/commands/info/userinfo.ts
+++ src/commands/info/userinfo.ts
@@ -9,23 +9,27 @@ module.exports = {
     let ping = msg.mentions.members.first();
 
     let roles = "";
-    let created;
-    let joined;
     let embed;
 
     if (ping) {
       ping.roles.cache.map((r) =>
         r.name != "@everyone" ? (roles += `${r} `) : ""
       );
-      created = ping.user.createdAt.toString().replace(/\([^)]*\)/g, "");
-      joined = ping.joinedAt.toString().replace(/\([^)]*\)/g, "");
       embed = new MessageEmbed()
         .setAuthor(`${ping.user.tag}`)
         .setColor("#47a8e8")
         .setDescription(`${ping}`)
         .addFields(
-          { name: "Created", value: `${created}`, inline: true },
-          { name: "Joined", value: `${joined}`, inline: true },
+          {
+            name: "Created",
+            value: `<t:${ping.user.createdAt.getTime()}:f>`,
+            inline: true,
+          },
+          {
+            name: "Joined",
+            value: `<t:${ping.joinedAt.getTime()}:f>`,
+            inline: true,
+          },
           { name: "Nickname", value: `${ping.displayName}`, inline: true },
           {
             name: "Roles",
@@ -40,15 +44,21 @@ module.exports = {
       msg.member.roles.cache.map((r) =>
         r.name != "@everyone" ? (roles += `${r} `) : ""
       );
-      created = msg.author.createdAt.toString().replace(/\([^)]*\)/g, "");
-      joined = msg.member.joinedAt.toString().replace(/\([^)]*\)/g, "");
       embed = new MessageEmbed()
         .setAuthor(`${msg.author.tag}`)
         .setColor("#47a8e8")
         .setDescription(`${msg.member}`)
         .addFields(
-          { name: "Created", value: `${created}`, inline: true },
-          { name: "Joined", value: `${joined}`, inline: true },
+          {
+            name: "Created",
+            value: `<t:${msg.author.createdAt.getTime()}:f>`,
+            inline: true,
+          },
+          {
+            name: "Joined",
+            value: `<t:${msg.member.joinedAt.getTime()}:f>`,
+            inline: true,
+          },
           {
             name: "Nickname",
             value: `${msg.member.displayName}`,
