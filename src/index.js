5754c862752d5143b27ea864c666cb0f7cc057cb
Chr1sDev <christopheranderson3@me.com>
2021-11-10 04:41:18 +0000

    [Index.js] Fix status disappearing cause of discord backend updating whatever

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const { token, prefix } = require("../config.json");
 client.on(`ready`, () => {
   console.log(`Logged in as ${client.user.tag}`);
   // client.user.setActivity(`/help`, { type: "WATCHING" });
-  client.user.setActivity(`frogge`, { type: "WATCHING" });
+  setInterval(() => {
+    client.user.setActivity(`frogge`, { type: "WATCHING" });
+  }, 60 * 60 * 1000); // 1hr
 
   // Run setup
   const files = fs
