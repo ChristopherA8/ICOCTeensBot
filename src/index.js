70a255760cc98225f8be3cb66e84b64c6ad90420
Chr1sDev <christopheranderson3@me.com>
2021-11-10 04:25:12 +0000

    tfw u realize you were gitignoring index.js ☠

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: Object.values(Intents.FLAGS) });
+const fs = require("fs");
+const { token, prefix } = require("../config.json");
+
+client.on(`ready`, () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  // client.user.setActivity(`/help`, { type: "WATCHING" });
+  client.user.setActivity(`frogge`, { type: "WATCHING" });
+
+  // Run setup
+  const files = fs
+    .readdirSync(`./src/setup`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of files) {
+    const { setup } = require(`../src/setup/${file}`);
+    setup(client);
+  }
+
+  // Run events
+  const eventfiles = fs
+    .readdirSync(`./src/events`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of eventfiles) {
+    const { event } = require(`../src/events/${file}`);
+    event(client);
+  }
+});
+
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+
+  // rules channel
+  if (msg.content !== "!rules" && msg.channel.id == "770730379077353494")
+    return msg.delete();
+
+  // Message Listeners
+  const files = fs
+    .readdirSync(`./src/listeners`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of files) {
+    const { listen } = require(`../src/listeners/${file}`);
+    await listen(msg);
+  }
+
+  const { commandHandler } = require("./handlers/commands.js");
+  commandHandler(msg, prefix);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.isCommand()) {
+    const { slashHandler } = require("./handlers/slash.js");
+    slashHandler(interaction, client);
+  }
+
+  if (interaction.isSelectMenu()) {
+    const { menuHandler } = require("./handlers/menus.js");
+    await menuHandler(interaction, client);
+  }
+
+  if (interaction.isButton()) {
+    const { buttonHandler } = require("./handlers/buttons.js");
+    buttonHandler(interaction, client);
+  }
+});
+
+client.login(token);
