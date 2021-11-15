a71579fba8912468618aa3e1c82cebeee85ceca7
Chr1sDev <christopheranderson3@me.com>
2021-11-15 04:08:36 +0000

    fix ratelimiting some other time

--- src/events/rateLimit.js
+++ src/events/rateLimit.js
@@ -1,9 +1,9 @@
 module.exports = {
   event(client) {
-    let x = 0;
-    client.on("rateLimit", (data) => {
-      console.log(x + " " + JSON.stringify(data, null, 2));
-      x++;
-    });
+    // let x = 0;
+    // client.on("rateLimit", (data) => {
+    //   console.log(x + " " + JSON.stringify(data, null, 2));
+    //   x++;
+    // });
   },
 };
