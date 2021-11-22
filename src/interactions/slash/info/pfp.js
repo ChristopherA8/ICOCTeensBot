ab2cd3f90ca11e938e20bf3d23328ed13b60f8a7
Chr1sDev <christopheranderson3@me.com>
2021-11-22 06:53:30 +0000

    [Slash] add option to get other people's pfp

--- src/interactions/slash/info/pfp.js
+++ src/interactions/slash/info/pfp.js
@@ -3,11 +3,20 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("pfp")
-    .setDescription("Fetch users profile picture"),
+    .setDescription("Fetch users profile picture")
+    .addUserOption((option) =>
+      option.setName("member").setDescription("Other member").setRequired(false)
+    ),
   permissions: 1,
   async execute(interaction) {
-    await interaction.reply(
-      interaction.user.displayAvatarURL({ dynamic: true })
-    );
+    let person = interaction.options.getMember("member");
+
+    if (!person) {
+      await interaction.reply(
+        interaction.user.displayAvatarURL({ dynamic: true })
+      );
+    } else {
+      await interaction.reply(person.user.displayAvatarURL({ dynamic: true }));
+    }
   },
 };
