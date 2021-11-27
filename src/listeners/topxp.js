21cf9b470b56d8a3f30a5647713463a856f59ae5
Chr1sDev <christopheranderson3@me.com>
2021-11-27 08:30:16 +0000

    [Listeners Top XP] use .env

--- src/listeners/topxp.js
+++ src/listeners/topxp.js
@@ -5,15 +5,21 @@ module.exports = {
 
     const topMem = await msg.guild.members.cache.get(`${person.user}`);
 
-    if (!topMem.roles.cache.some((role) => role.id == `808429363392806952`)) {
+    if (
+      !topMem.roles.cache.some(
+        (role) => role.id == process.env.XP_LEADER_ROLE_ID
+      )
+    ) {
       msg.guild.members.cache
         .filter((mem) =>
-          mem.roles.cache.some((role) => role.id == `808429363392806952`)
+          mem.roles.cache.some(
+            (role) => role.id == process.env.XP_LEADER_ROLE_ID
+          )
         )
         .forEach((member) => {
-          member.roles.remove(`808429363392806952`);
+          member.roles.remove(process.env.XP_LEADER_ROLE_ID);
         });
-      topMem.roles.add(`808429363392806952`);
+      topMem.roles.add(process.env.XP_LEADER_ROLE_ID);
     }
   },
 };
