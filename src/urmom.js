e6a221bf2e06cdbf801d7bb7f1983ac05fcf5ecf
Chr1sDev <christopheranderson3@me.com>
2021-12-27 20:56:16 +0000

    .

--- src/urmom.js
+++ src/urmom.js
@@ -3,6 +3,7 @@ const CommandManager = require("./InteractionManagers/CommandManager");
 CommandManager.addCommand({
   name: "urmom",
   async execute(msg) {
+    msg.reply("urmom");
     CommandManager.log("urmom", "urmom");
   },
 });
