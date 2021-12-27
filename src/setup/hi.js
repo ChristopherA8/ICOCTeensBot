4241747281b053c9a34838dba784605710b14ea4
Chr1sDev <christopheranderson3@me.com>
2021-12-27 17:21:27 +0000

    .

--- src/setup/hi.js
+++ src/setup/hi.js
@@ -1,14 +1,14 @@
 module.exports = {
   setup(client) {
     const guild = client.guilds.cache.get("698590629344575500");
-    const channel = guild.channels.cache.get("698590629344575503");
+    const channel = guild.channels.cache.get("768843277406568499");
     // channel.send(
     //   "https://cdn.discordapp.com/emojis/665980185186795540.gif?size=96"
     // );
     // channel.messages
-    //   .fetch("924105197767249950")
+    //   .fetch("924482354561568808")
     //   .then((message) => {
-    //     message.reply("🤨");
+    //     message.reply("POG");
     //   })
     //   .catch(console.error);
   },
